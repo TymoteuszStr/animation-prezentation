815fa69657bb339e754eec00971286efb34072e7
Tymek <tymoteusz.strozniak@gmail.com>
2021-08-22 18:23:43 +0200

    project init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+<Slajd/>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Slajd from '@/components/Slajd.vue'
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
-  },
+    Slajd
+ },
 };
 </script>
