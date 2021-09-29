08bbbfe0afa786c7c2a0885f029ba49edf37fb7a
Tymoteusz <tymoteusz.strozniak@futurum.tech>
2021-09-29 16:06:10 +0200

    add telegram

--- src/components/Slajd.vue
+++ src/components/Slajd.vue
@@ -1,31 +1,30 @@
+<!-- @format -->
+
 <template>
-  <div class="wrapper">
-    <div class="slajd-container">TE</div>
-  </div>
+	<div class="wrapper">
+		<div class="slajd-container">Telegram</div>
+	</div>
 </template>
 
 <script>
-
-
-export default {
-  name: "Home",
-  components: {
- },
-};
+	export default {
+		name: "Home",
+		components: {},
+	}
 </script>
 
 <style lang="scss">
-.wrapper{
-    width: 100vw;
-    height: 100vh;
-    border: 10px solid black;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-}
-.slajd-container{
-    border: 10px solid red;
-    width:1000px;
-    height: 600px;
-}
+	.wrapper {
+		width: 100vw;
+		height: 100vh;
+		border: 10px solid black;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+	.slajd-container {
+		border: 10px solid red;
+		width: 1000px;
+		height: 600px;
+	}
 </style>
