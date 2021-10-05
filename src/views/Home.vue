88bcec82426378ab7a98a3e7a838ff6805cfdb0f
dorotawejdman <dorota.wejdman@gmail.com>
2021-10-05 19:07:45 +0200

    Add buttons component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <ZegarSlajd1 />
   <Bg3DSlajd1 />
+  <Buttons />
 </template>
 
 <script>
 import ZegarSlajd1 from "@/components/Slajdy/Zegar/ZegarSlajd1.vue";
 import Bg3DSlajd1 from "@/components/Slajdy/Background3D/Bg3DSlajd1.vue";
+import Buttons from "@/components/Slajdy/Buttons/Buttons.vue";
 
 export default {
   name: "Home",
   components: {
     ZegarSlajd1,
     Bg3DSlajd1,
+    Buttons
   },
 };
 </script>
